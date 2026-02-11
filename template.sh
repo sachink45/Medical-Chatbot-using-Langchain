f5e0046d0535e6d70e1af47326abbef926d269c5
Sachin Kapase <sachinkapase6125@gmail.com>
2026-02-11 12:02:08 +0000

    folder structure

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p resources
+
+
+touch src/main.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "done"
